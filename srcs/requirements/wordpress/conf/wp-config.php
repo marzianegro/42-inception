158768ba44466e7bd0d9e4dca4945c0051c11eef
marzianegro <ngr.marzia@gmail.com>
2024-07-04 16:32:31 +0200

    pushino per te (you know who you are)

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,10 +1,20 @@
 <?php
 
 # Website's base configuration details
-define( 'DB_NAME', getenv('MARIADB_DATABASE') );
-define( 'DB_USER', getenv('MARIADB_USER') );
-define( 'DB_PASSWORD', getenv('MARIADB_PASSWORD') );
-define( 'DB_HOST', 'mariadb:3306' ); 
 
 define('WP_HOME', 'https://mnegro.42.fr');
 define('WP_SITEURL', 'https://mnegro.42.fr');
+
+// define('DB_NAME', 'test_db');
+// define('DB_USER', 'test_user');
+// define('DB_PASSWORD', 'test_pw');
+// define('DB_HOST', 'mariadb_test:3306');
+// define('DB_CHARSET', 'utf8mb4');
+// define('DB_COLLATE', '');
+
+define('DB_NAME', 'your_wp_db');
+define('DB_USER', 'your_wp_user');
+define('DB_PASSWORD', 'your_password');
+define('DB_HOST', 'mariadb_test:3306');
+define('DB_CHARSET', 'utf8mb4');
+define('DB_COLLATE', '');
