92b26f323ac36b544e6674eccf18c384890dd01c
marzianegro <ngr.marzia@gmail.com>
2024-07-07 16:42:35 +0200

    HELPPPP

--- srcs/requirements/wordpress/tools/wp-boot.sh
+++ srcs/requirements/wordpress/tools/wp-boot.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-# run WP-CLI install command
-# administrator user
-wp --path=/var/www/html/wordpress core install --url="https://${DOMAIN_NAME}" --title="${WP_TITLE}" \
-		--admin_user="${WP_OWNER_USER}" --admin_password="${WP_OWNER_PASSWORD}" --admin_email="${WP_OWNER_EMAIL}"
+# exit script on any error
+set -e
 
-# test user
-wp --path=/var/www/html/wordpress user create "${WP_GUEST_USER}" "${WP_GUEST_EMAIL}" \
-          --role=author --user_pass="${WP_GUEST_PASSWORD}"
+# check if WordPress is already installed
+if ! wp --path=/var/www/html/wordpress core is-installed; then
+  # run WP-CLI install command
+  # administrator user
+  wp --path=/var/www/html/wordpress core install --url="${DOMAIN_NAME}" --title="${WP_TITLE}" \
+          --admin_user="${WP_OWNER_USER}" --admin_password="${MYSQL_PASSWORD}" --admin_email="${WP_OWNER_EMAIL}"
+
+  # test user
+  wp --path=/var/www/html/wordpress user create "${WP_GUEST_USER}" "${WP_GUEST_EMAIL}" \
+            --role=author --user_pass="${MYSQL_PASSWORD}"
+else
+  echo "WordPress is already installed."
+fi
 
 exec php-fpm82 -F
