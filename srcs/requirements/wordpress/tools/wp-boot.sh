50fe623dd12a6b254b98fda3ea29b54c4e9afc44
marzianegro <ngr.marzia@gmail.com>
2024-07-07 16:01:38 +0200

    still not working rip

--- srcs/requirements/wordpress/tools/wp-boot.sh
+++ srcs/requirements/wordpress/tools/wp-boot.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 # run WP-CLI install command
 # administrator user
 wp --path=/var/www/html/wordpress core install --url="https://${DOMAIN_NAME}" --title="${WP_TITLE}" \
-		--admin_user="${WP_OWNER}" --admin_password="${WP_OWNER_PASSWORD}" --admin_email="${WP_OWNER_EMAIL}"
+		--admin_user="${WP_OWNER_USER}" --admin_password="${WP_OWNER_PASSWORD}" --admin_email="${WP_OWNER_EMAIL}"
 
 # test user
-wp --path=/var/www/html/wordpress user create "${WP_GUEST}" "${WP_GUEST_EMAIL}" \
+wp --path=/var/www/html/wordpress user create "${WP_GUEST_USER}" "${WP_GUEST_EMAIL}" \
           --role=author --user_pass="${WP_GUEST_PASSWORD}"
 
 exec php-fpm82 -F
